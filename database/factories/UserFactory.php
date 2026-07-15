22cac5de497526326e8a58f2bda328950472741b
TAHAMOUA <mouaddinetaha@gmail.com>
2026-07-15 12:31:49 +0100

    add factories uesrfactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,16 +23,21 @@ class UserFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-    {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ];
-    }
-
+{
+    return [
+        'prenom' => fake()->firstName(),
+        'nom' => fake()->lastName(),
+        'email' => fake()->unique()->safeEmail(),
+        'email_verified_at' => now(),
+        'password' => bcrypt('password'),
+        'role' => fake()->randomElement([
+            'admin',
+            'entreprise',
+            'candidat'
+        ]),
+        'remember_token' => Str::random(10),
+    ];
+}
     /**
      * Indicate that the model's email address should be unverified.
      */
