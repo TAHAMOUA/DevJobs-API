7e9724bab584abd475765455c5376990d0c85578
TAHAMOUA <mouaddinetaha@gmail.com>
2026-07-15 16:24:11 +0100

    feat(auth): install and configure Laravel Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
-
+    use HasApiTokens, HasFactory, Notifiable;
     protected $fillable = [
         'prenom',
         'nom',
@@ -31,6 +30,7 @@ class User extends Authenticatable
         ];
     }
 
+    
     public function entreprise()
     {
         return $this->hasOne(Entreprise::class);
