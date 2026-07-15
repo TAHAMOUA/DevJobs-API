2e547b0f211eaa4dd704dcd9bbd27a7b6f040b36
TAHAMOUA <mouaddinetaha@gmail.com>
2026-07-15 12:26:31 +0100

    add model candidatures

--- app/Models/Competence.php
+++ app/Models/Competence.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Competence extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nom',
+    ];
+
+    public function offres()
+    {
+        return $this->belongsToMany(Offre::class);
+    }
 }
--- app/Models/candidatures.php
+++ app/Models/candidatures.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class candidatures extends Model
+class Candidature extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'statut',
+        'date_candidature',
+        'user_id',
+        'offre_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function offre()
+    {
+        return $this->belongsTo(Offre::class);
+    }
 }
