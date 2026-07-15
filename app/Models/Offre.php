d2b032b4d5a17a6c45d182cba59a1429efdab48b
TAHAMOUA <mouaddinetaha@gmail.com>
2026-07-15 12:24:05 +0100

    add model user and entreprise and offre

--- app/Models/Offre.php
+++ app/Models/Offre.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Offre extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'titre',
+        'description',
+        'type_contrat',
+        'entreprise_id',
+    ];
+
+    public function entreprise()
+    {
+        return $this->belongsTo(Entreprise::class);
+    }
+
+    public function candidatures()
+    {
+        return $this->hasMany(Candidature::class);
+    }
+
+    public function competences()
+    {
+        return $this->belongsToMany(Competence::class);
+    }
 }
