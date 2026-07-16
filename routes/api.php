bf64f412524b53733d46d8e5330b6d9f17a802ea
TAHAMOUA <mouaddinetaha@gmail.com>
2026-07-16 12:36:33 +0100

    feat(auth): implement authentication with Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,16 @@ Route::get('/test', function () {
         'message' => 'API DevJobs works!'
     ]);
 });
+
+
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
+
